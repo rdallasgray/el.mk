b83e65177e319f857dd73c2b5402e4a2309a070b
Robert Dallas Gray <mail@robertdallasgray.com>
2013-03-31 14:36:53 +0100

    Include all *.el files in src/ when versioning.

--- el.mk
+++ el.mk
@@ -6,7 +6,7 @@ TAG=$(shell git describe --abbrev=0)
 REV=$(shell git describe --long | sed -E 's/([^\-]*)\-([0-9]+).*/\2/')
 VERSION=$(TAG).$(REV)
 YEAR=$(shell date +"%Y")
-VERSIONED_FILES=src-pkg.el $(PROJECT_LCNAME).el
+VERSIONED_FILES=$(shell cd src && ls *.el)
 COMMENTARY_FILE=README.md
 TEST_FILE=test/$(PROJECT_LCNAME)-test-main.el
 
