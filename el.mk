22a5b42c5402f207faaaaf15d4cfafde4c2e1725
Robert Dallas Gray <robert.dallas.gray@huzutech.com>
2013-04-26 09:39:24 +0100

    Removed in-place sed commands.

--- el.mk
+++ el.mk
@@ -2,13 +2,13 @@ SRC_DIR=src
 BUILD_DIR=lib
 TMP_DIR=tmp
 
-TAG=$(shell git describe --abbrev=0)
-REV=$(shell git describe --long | sed -E 's/([^\-]*)\-([0-9]+).*/\2/')
-VERSION=$(TAG).$(REV)
-YEAR=$(shell date +"%Y")
-VERSIONED_FILES=$(shell cd src && ls *.el)
+TAG=${shell git describe --abbrev=0}
+REV=${shell git describe --long | sed -E 's/([^\-]*)\-([0-9]+).*/\2/'}
+VERSION=${TAG}.${REV}
+YEAR=${shell date +"%Y"}
+VERSIONED_FILES=${shell cd src && ls *.el}
 COMMENTARY_FILE=README.md
-TEST_FILE=test/$(PROJECT_LCNAME)-test-main.el
+TEST_FILE=test/${PROJECT_LCNAME}-test-main.el
 
 all: build-cleanup
 
@@ -34,10 +34,9 @@ build-cleanup: build
 	@`rm lib/Carton`
 
 version: setup carton
-	@for FILE in $(VERSIONED_FILES); do \
-	echo "Setting version number ${VERSION} and year ${YEAR} in ${TMP_DIR}/$$FILE"; \
-	sed -i '' -e 's/@VERSION/${VERSION}/g' ${TMP_DIR}/$$FILE; \
-	sed -i '' -e 's/@YEAR/${YEAR}/g' ${TMP_DIR}/$$FILE; \
+	@for FILE in ${VERSIONED_FILES}; do \
+	echo "Setting version number ${VERSION} and year ${YEAR} in ${SRC_DIR}/$$FILE"; \
+	sed -e 's/@VERSION/${VERSION}/g' -e 's/@YEAR/${YEAR}/g' ${SRC_DIR}/$$FILE > ${TMP_DIR}/$$FILE; \
 	done
 
 carton:
@@ -45,16 +44,17 @@ carton:
 	@`cd src && carton package`
 
 rename-package: setup carton version
-	@echo "Renaming tmp/src-pkg.el to tmp/$(PROJECT_LCNAME)-pkg.el"
-	@`mv -f ${TMP_DIR}/src-pkg.el ${TMP_DIR}/$(PROJECT_LCNAME)-pkg.el`
+	@echo "Renaming tmp/src-pkg.el to tmp/${PROJECT_LCNAME}-pkg.el"
+	@`mv -f ${TMP_DIR}/src-pkg.el ${TMP_DIR}/${PROJECT_LCNAME}-pkg.el`
 
 commentary: setup
 	@echo "Inserting commentary"
-	@sed 's/^/;; /' < ${COMMENTARY_FILE} > ${TMP_DIR}/commentary
-	@sed -i '' -e '/@COMMENTARY/r ${TMP_DIR}/commentary' -e '//d' ${TMP_DIR}/$(PROJECT_LCNAME).el
+	@sed 's/^/;; /' ${COMMENTARY_FILE} > ${TMP_DIR}/commentary
+	@sed -e '/@COMMENTARY/r ${TMP_DIR}/commentary' -e '//d' ${TMP_DIR}/${PROJECT_LCNAME}.el > ${TMP_DIR}/${PROJECT_LCNAME}_commented.el
+	@mv ${TMP_DIR}/${PROJECT_LCNAME}_commented.el ${TMP_DIR}/${PROJECT_LCNAME}.el
 
 test: build-cleanup
-	@`emacs -batch -l ert -l $(TEST_FILE) -f ert-run-tests-batch-and-exit`
+	@`emacs -batch -l ert -l ${TEST_FILE} -f ert-run-tests-batch-and-exit`
 
 build: setup rename-package commentary
 	@`cp tmp/* lib/`
